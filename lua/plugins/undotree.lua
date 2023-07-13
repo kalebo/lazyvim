92c5600f104e746ada5aa3deda0a88e0e63d0615
Kaleb Olson <kaleb@coreform.com>
2023-07-13 17:16:47 -0600

    cust: add undotree

---
+++ lua/plugins/undotree.lua
@@ -0,0 +1,8 @@
+return {
+  -- branches everywhere
+  {
+    "mbbill/undotree",
+    cmd = "UndotreeToggle",
+    keys = { { "<leader>ou", "<cmd>UndotreeToggle<cr><cmd>UndotreeFocus<cr>", desc = "Open undotree" } },
+  },
+}
